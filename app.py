ddb23044925b736cb3536dfb4f054398636b85e0
Tomas Brejl <tomas.brejl@outlook.com>
2021-02-15 09:06:13 -0800

    connected to outside database

--- app.py
+++ app.py
@@ -5,13 +5,24 @@ import pymysql
 app = Flask(__name__)
 
 test_db = pymysql.connect(
-    host="localhost",
-    user="root",
-    password="greatkid5000",
-    database="sys"
+    host="sql3.freesqldatabase.com",
+    user="sql3393154",
+    password="p3tsfT6ePr",
+    database="sql3393154"
 )
 
 cursor = test_db.cursor()
+
+cursor.execute("CREATE TABLE people IF NOT EXISTS (id INT AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(150), lastname VARCHAR(150))")
+sql = "INSTERT INTO customers (firstname, lastname) VALUES (%s, %s)"
+val = [
+    ("Tomas", "Brejl")
+    ("Lucie", "Brejlova")
+]
+
+cursor.executemany(sql, val)
+
+mydb.commit()
 
 @app.route('/')
 def hello_world():
